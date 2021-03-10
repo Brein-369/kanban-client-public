9f17533aca6ea59f9acfacac27980d78a53b6452
Brein-369 <edwardbrainerd@gmail.com>
2021-03-10 11:33:33 +0700

    front end client only kanban home

--- main.js
+++ main.js
@@ -1,13 +1,48 @@
 const app = new Vue({
-  el:"#app",
-  data:{
-    logo: "KanbanFox",
-    username: "<h2>Welcome Jack</h2>",
-    page: "todo-page",
-  },
-  methods:{ 
-    changePage(to){
-      this.page = to;
+    el : '#app',
+    data : {
+        page : "home",
+        products : [
+            {   
+                id : 1,
+                name : "product 1",
+                task : "task1"
+            },
+            {
+                id : 2,
+                name : "product 2",
+                task : "task2"
+            },
+            {
+                id : 3,
+                name : "product 3",
+                task : "task3"
+            },
+            {
+                id : 4,
+                name : "product 4",
+                task : "task4"
+            },
+            {
+                id : 5,
+                name : "product 5",
+                task : "task5"
+            }
+        ]
+    },
+    methods :{
+        deleteProduct(id){
+            console.log(id);
+            this.products = this.products.filter((e,i)=>{
+                console.log(i);
+                if(e.id !== id){
+                    console.log((e.id, id));
+                    return e
+                }
+            })
+            return this.products
+        }
     }
-  }
+
+
 })
